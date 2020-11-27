056c5d5a625526e215d5ebb4fb1f0119850a7b12
Larkin <liu.larkin@gmail.com>
2020-11-27 21:56:59 +0100

    new dir path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,9 +9,13 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenLocal()
     mavenCentral()
+    flatDir {dirs = setOf(file("libs")) }
 }
 dependencies {
-    implementation("org.example:lib:1.0-SNAPSHOT")
+    implementation( "org.example:lib:1.0-SNAPSHOT"  )
+    // implementation files("libs/org.example:lib:1.0-SNAPSHOT")
+    // implementation files("libs/lib-1.0-SNAPSHOT.jar")
+    // fileTree(dir: "libs", include: "*.jar")
     testImplementation(kotlin("test-junit"))
 }
 tasks.withType<KotlinCompile>() {
