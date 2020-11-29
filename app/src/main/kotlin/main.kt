d2c2885e0357dbb811ea26daa063d5404e0fd8e8
Larkin <liu.larkin@gmail.com>
2020-11-29 23:38:11 +0100

    added visualizer

--- app/src/main/kotlin/main.kt
+++ app/src/main/kotlin/main.kt
@@ -2,6 +2,7 @@ import kotlin.random.Random
 import GridWorldGridSolve
 
 fun main() {
+
     val gw = GridWorldGridSolve(
         4,
         4,
@@ -11,8 +12,11 @@ fun main() {
             // GridworldReward(3, 3, 1.0),
             GridworldReward(1, 1, 0.3)
         ),
-        0.6)
+        1.0)
 
     gw.getWorldSolve()
+    gw.visualizeWorldSolve()
+
+
 
 }
