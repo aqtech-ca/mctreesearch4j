939589e9038ba00343f689a8826d8e4ba44f9533
larkz <liu.larkin@gmail.com>
2021-01-09 00:13:39 +0100

    Added in GridWorld unit tests.

--- app/src/test/kotlin/GridWorldTests.kt
+++ app/src/test/kotlin/GridWorldTests.kt
@@ -8,12 +8,37 @@ import org.junit.jupiter.api.Assertions.assertTrue
 
 class GridWorldTests {
 
-    fun testOpenSpaceGriwWorld(): Double {
+    val correctSolutionsOpenSpace = arrayOf(
+            arrayOf(arrayOf("↑", "→"), arrayOf("↑")  , arrayOf("←", "↑"), arrayOf("←", "↑"), arrayOf("←", "↑"), arrayOf("←", "↑"), arrayOf("←", "↑"), arrayOf("←", "↑")),
+            arrayOf(arrayOf("→")     , arrayOf("R")  , arrayOf("←")     , arrayOf("←")     , arrayOf("←")     , arrayOf("←")     , arrayOf("←")     , arrayOf("←")     ),
+            arrayOf(arrayOf("↓", "→"), arrayOf("↓")  , arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓")),
+            arrayOf(arrayOf("↓", "→"), arrayOf("↓")  , arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓")),
+            arrayOf(arrayOf("↓", "→"), arrayOf("↓")  , arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("X")     , arrayOf("←", "↓"), arrayOf("←", "↓")),
+    )
+
+    val worldFeaturesOpenSpace = listOf(
+            GridworldReward(5, 4, -0.5),
+            GridworldReward(1, 1, 1.0)
+    )
+
+    val correctSolutionsWall = arrayOf(
+            arrayOf(arrayOf("↑", "→"), arrayOf("↑")  , arrayOf("←", "↑"), arrayOf("←", "↑"), arrayOf("←", "↑"), arrayOf("X")     , arrayOf("→", "↑"), arrayOf("←", "↑")),
+            arrayOf(arrayOf("→")     , arrayOf("R")  , arrayOf("←")     , arrayOf("←")     , arrayOf("←")     , arrayOf("X")     , arrayOf("→", "↑"), arrayOf("→", "↑")),
+            arrayOf(arrayOf("↓", "→"), arrayOf("↓")  , arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("X")     , arrayOf("→", "↑"), arrayOf("←", "↑")),
+            arrayOf(arrayOf("↓", "→"), arrayOf("↓")  , arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("X")     , arrayOf("→", "↑"), arrayOf("←", "↑")),
+            arrayOf(arrayOf("↓", "→"), arrayOf("↓")  , arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←")     , arrayOf("←"     ), arrayOf("←")    ),
+    )
+
+    val worldFeaturesWall = listOf(
+            GridworldReward(5, 3, -0.1),
+            GridworldReward(5, 2, -0.1),
+            GridworldReward(5, 1, -0.1),
+            GridworldReward(5, 0, -0.1),
+            GridworldReward(1, 1, 1.0)
+    )
+
+    fun testGridWorld(worldFeatures: List<GridworldReward>, solutionArray: Array<Array<Array<String>>>): Double {
         // Open space small world test.
-        val worldFeatures = listOf(
-                GridworldReward(5, 4, -0.5),
-                GridworldReward(1, 1, 1.0)
-        )
 
         val gw = GridWorldGridSolve(
                 8,
@@ -27,40 +52,31 @@ class GridWorldTests {
                 false
         )
 
-        // Correct solutions
-
-        val correctSolutions = arrayOf(
-                arrayOf(arrayOf("↑", "→"), arrayOf("↑")  , arrayOf("←", "↑"), arrayOf("←", "↑"), arrayOf("←", "↑"), arrayOf("←", "↑"), arrayOf("←", "↑"), arrayOf("←", "↑")),
-                arrayOf(arrayOf("→")     , arrayOf("R")  , arrayOf("←")     , arrayOf("←")     , arrayOf("←")     , arrayOf("←")     , arrayOf("←")     , arrayOf("←")     ),
-                arrayOf(arrayOf("↓", "→"), arrayOf("↓")  , arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓")),
-                arrayOf(arrayOf("↓", "→"), arrayOf("↓")  , arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓")),
-                arrayOf(arrayOf("↓", "→"), arrayOf("↓")  , arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("←", "↓"), arrayOf("X")     , arrayOf("←", "↓"), arrayOf("←", "↓")),
-        )
-
         gw.getWorldSolve()
         gw.visualizeWorldSolve()
 
         var numIncorrect = 0.0
         for (s in gw.mapOfSolutions) {
             var index = s.key
-            if (!correctSolutions[index.second][index.first].contains(gw.mapOfSolutions[index])){
+            if (!solutionArray[index.second][index.first].contains(gw.mapOfSolutions[index])){
                 numIncorrect += 1
             }
         }
 
-        // The open world solve should be 90% accurate
-
-        // println(gw.mapOfSolutions)
-        // println(worldFeatures.size)
-        // println(gw.mapOfSolutions.size)
-        // println(numIncorrect)
+        println(gw.mapOfSolutions)
+        println(worldFeatures.size)
+        println(gw.mapOfSolutions.size)
+        println(numIncorrect)
         return numIncorrect/ (gw.mapOfSolutions.size - worldFeatures.size)
 
     }
 
     @Test fun gridWorldTests() {
-        val openSpaceGridWorldError = this.testOpenSpaceGriwWorld()
-        assertTrue(openSpaceGridWorldError < 0.05 , "Error percentage for grid world solution, " + openSpaceGridWorldError.toString() + " must be less than: 0.05")
-    }
+        // The world solves should be 95% accurate
+        val openSpaceGridWorldError = this.testGridWorld(this.worldFeaturesOpenSpace, this.correctSolutionsOpenSpace)
+        assertTrue(openSpaceGridWorldError < 0.05 , "Error percentage for grid world solution, (open space) " + openSpaceGridWorldError.toString() + " must be less than: 0.05")
 
+        val wallError = this.testGridWorld(this.worldFeaturesWall, this.correctSolutionsWall)
+        assertTrue(wallError < 0.05 , "Error percentage for grid world solution, (wall) " + wallError.toString() + " must be less than: 0.05")
+    }
 }
