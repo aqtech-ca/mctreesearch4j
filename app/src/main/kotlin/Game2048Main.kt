3d0d9eb020debd763200de0e0d28004086819785
larkz <liu.larkin@gmail.com>
2021-01-09 00:45:55 +0100

    Allow game board to start with different initial states for unit testing.

--- app/src/main/kotlin/Game2048Main.kt
+++ app/src/main/kotlin/Game2048Main.kt
@@ -1,16 +1,33 @@
 import Twenty48.Game2048MDP
 import kotlin.random.Random
 import StatefulSolver
+import Twenty48.Game2048Position
+import Twenty48.Game2048State
 
 fun main() {
 
-    val game2048MDP = Game2048MDP()
+    /*
+    val initialGameState = Game2048State(Game2048Position(arrayOf(
+            arrayOf(1024, 0, 0, 0),
+            arrayOf(512, 512, 0, 0),
+            arrayOf(0, 0, 0, 0),
+            arrayOf(0, 0, 0, 0)
+    )))
+    */
+    val initialGameState = Game2048State(Game2048Position(arrayOf(
+            arrayOf(0, 0, 0, 0),
+            arrayOf(0, 0, 0, 0),
+            arrayOf(64, 64, 0, 0),
+            arrayOf(128, 256, 512, 1024)
+    )))
 
-    var solver = StatefulSolver(
+    val game2048MDP = Game2048MDP(initialGameState)
+
+    var solver = StatelessSolver(
         game2048MDP,
         Random,
-        1000,
-        999,
+        20000,
+        60,
         1.4,
         0.9,
         true
