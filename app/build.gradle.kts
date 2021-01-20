3f00ad9dadff43f5cd795d238af1d3bbb8a44fc1
John Luo <jt.luo@mail.utoronto.ca>
2021-01-19 23:16:39 -0800

    Cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,9 +24,6 @@ dependencies {
     testImplementation(kotlin("test-junit"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.1.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.1.0")
-
-    // testImplementation(platform("org.junit:junit-bom:5.7.0"))
-    // testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.named<Test>("test") {
