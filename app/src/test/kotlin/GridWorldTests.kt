29b4bd1d2ebdbb036a8d76eac8b43cf831587d75
larkz <liu.larkin@gmail.com>
2021-06-20 21:13:25 +0200

    changed pass rate back to 15%

--- app/src/test/kotlin/GridWorldTests.kt
+++ app/src/test/kotlin/GridWorldTests.kt
@@ -79,7 +79,7 @@ class GridWorldTests {
             openWorldErrors.add(this.testGridWorld(this.worldFeaturesOpenSpace, this.correctSolutionsOpenSpace))
         }
 
-        assertTrue(openWorldErrors.average() < 0.7, "Error percentage for grid world solution, (open space) " + openWorldErrors.average().toString() + " must be less than: 0.05")
+        assertTrue(openWorldErrors.average() < 0.15, "Error percentage for grid world solution, (open space) " + openWorldErrors.average().toString() + " must be less than: 0.05")
     }
     @Test fun gridWorldTestWall() {
         var wallErrorValues = mutableListOf(this.testGridWorld(this.worldFeaturesWall, this.correctSolutionsWall))
@@ -87,6 +87,6 @@ class GridWorldTests {
             wallErrorValues.add(this.testGridWorld(this.worldFeaturesWall, this.correctSolutionsWall))
         }
 
-        assertTrue(wallErrorValues.average() < 0.7 , "Error percentage for grid world solution, (wall) " + wallErrorValues.average() + " must be less than: 0.05")
+        assertTrue(wallErrorValues.average() < 0.15 , "Error percentage for grid world solution, (wall) " + wallErrorValues.average() + " must be less than: 0.05")
     }
 }
