ac248cd66c4508b3d84d9e9300b940ae37ca5513
larkz <liu.larkin@gmail.com>
2021-06-21 23:50:18 +0200

    renamed to MDP<StateType, ActionType>

--- lib/src/main/kotlin/ca/aqtech/mctreesearch4j/MDP.kt
+++ lib/src/main/kotlin/ca/aqtech/mctreesearch4j/MDP.kt
@@ -1,9 +1,9 @@
 package ca.aqtech.mctreesearch4j
 
-abstract class MDP<TState, TAction> {
-    abstract fun transition(state: TState, action: TAction): TState
-    abstract fun reward(previousState: TState?, action: TAction?, state: TState): Double
-    abstract fun initialState(): TState
-    abstract fun isTerminal(state: TState): Boolean
-    abstract fun actions(state: TState): Collection<TAction>
+abstract class MDP<StateType, ActionType> {
+    abstract fun transition(state: StateType, action: ActionType): StateType
+    abstract fun reward(previousState: StateType?, action: ActionType?, state: StateType): Double
+    abstract fun initialState(): StateType
+    abstract fun isTerminal(state: StateType): Boolean
+    abstract fun actions(state: StateType): Collection<ActionType>
 }
