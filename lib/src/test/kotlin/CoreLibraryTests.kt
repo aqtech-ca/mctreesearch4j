d11fea0cd7c83d88c5af6fffca8dd53646b346b2
larkz <liu.larkin@gmail.com>
2021-10-17 00:15:01 +0200

    added all tests, to test simulation

--- lib/src/test/kotlin/CoreLibraryTests.kt
+++ lib/src/test/kotlin/CoreLibraryTests.kt
@@ -17,7 +17,7 @@ class CoreLibraryTests {
     val depthLimit = 29
     val exploreConstant = 0.4
     val rewardDiscount = 0.01
-    val verbose = true
+    val verbose = false
 
     val solver = GenericSolver(
         testMDP,
@@ -37,9 +37,10 @@ class CoreLibraryTests {
     }
 
     // Test selection returns an action node.
-    @Test fun coreLibraryTestSelection() {
+    @Test fun coreLibraryTestSelection(): ActionNode<TestStochasticState, TestStochasticAction>  {
         val node = solver.select(testRoot)
         assertTrue(node is ActionNode<TestStochasticState, TestStochasticAction>,  "Test Search Tree Selection")
+        return node
     }
 
     // Ensure expansion is working properly, and meta-info is transfered from state-to-state.
@@ -54,31 +55,51 @@ class CoreLibraryTests {
     }
 
     // Test selection returns an action node.
-    @Test fun coreLibraryTestSelection2() {
+    @Test fun coreLibraryTestSelection2(): ActionNode<TestStochasticState, TestStochasticAction> {
         val node = solver.select(coreLibraryTestExpand())
         assertTrue(node is ActionNode<TestStochasticState, TestStochasticAction>,  "Test Search Tree Selection")
+        return node
     }
 
-    // Test simulation
+    // Test simulation returns a valid numeric reward
     @Test fun coreLibraryTestSimulation() {
         val rewardValue = solver.simulate(testRoot)
         assertTrue(rewardValue is Double,  "Test Simulation")
     }
 
-    // Test backpropagation
+    // Test backpropagation function increment
     @Test fun coreLibraryTestBackpropagation() {
         var node = testRoot
         val n1 = node.n
         solver.backpropagate(testRoot, 20.0)
         val n2 = node.n
+        println("n2: " + n2.toString())
+        println("n1: " + n1.toString())
         assertTrue(n2 > n1,  "Test Increment of Node Visits")
     }
 
     // Tests for the base Solver
 
-    // Test if counted n times properly
+    // Test all key mechanisms, run single iteration
+    @Test fun coreLibraryTestSingleIteration() {
+        solver.runTreeSearchIteration()
+    }
 
+    // Test MC Tree Search, test if counted n times properly
+    @Test fun coreLibraryTestMCTS() {
+        solver.runTreeSearch(99)
+        val rootN = testRoot.n
+        println("n root: "+ rootN.toString())
 
-
-
+        var nextNodes = testRoot.getChildren() ?: return
+        var n1 = testRoot.n
+        while(!nextNodes.isEmpty()){
+            var nextNode = nextNodes.random()
+            var n2 = nextNode.n
+            println("next node n: " + n2.toString())
+            nextNodes = nextNode.getChildren()
+            assertTrue(n2 < n1,  "Test Increment of Node Visits")
+            n1 = n2
+        }
+    }
 }
