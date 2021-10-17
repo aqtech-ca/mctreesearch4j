821e4b3e95672e28daa6f0c42891dae81a879f10
larkz <liu.larkin@gmail.com>
2021-10-17 23:45:32 +0200

    updated documentatio on unit tests

--- lib/src/test/kotlin/CoreLibraryTests.kt
+++ lib/src/test/kotlin/CoreLibraryTests.kt
@@ -1,19 +1,19 @@
-
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import TestStochasticMDP
 import ca.aqtech.mctreesearch4j.ActionNode
 import ca.aqtech.mctreesearch4j.GenericSolver
-import TestStochasticAction
-import TestStochasticState
+
+/**
+ * A set of unit tests to test the mctreesearch4j package. A simple MDP is designed [TestStochasticMDP].
+ *
+ * A set of tests is desigined for the [GenericSolver] and the base class [Solver]. The tests ensure proper implementation
+ * of the vital classes.
+ *
+ */
 
 class CoreLibraryTests {
-    // Test Node Expand
-    // var root = ActionNode<TestStochasticState, TestStochasticAction>(null, null)
-    // Test Simple MDP Function
 
     val testMDP = TestStochasticMDP()
-
     val depthLimit = 29
     val exploreConstant = 0.4
     val rewardDiscount = 0.01
@@ -27,23 +27,42 @@ class CoreLibraryTests {
         verbose
     )
 
-    // Testing the GenericSolver (Default)
     var testRoot = solver.root
 
-    // Ensure State content is not mutated in transition
-    @Test fun coreLibraryStateMutationCheck() {
-        val testNode = solver.expand(testRoot)
-        assertTrue(testNode.state.stateIndex is Int,  "Is integer, state content unmutated.")
+    // Generic Solver Testing
+    /**
+     * Tests the [GenericSolver.expand] method functionality without failure.
+     */
+    @Test fun coreLibraryTestExpandMethod() {
+        solver.expand(testRoot)
     }
 
-    // Test selection returns an action node.
-    @Test fun coreLibraryTestSelection(): ActionNode<TestStochasticState, TestStochasticAction>  {
-        val node = solver.select(testRoot)
-        assertTrue(node is ActionNode<TestStochasticState, TestStochasticAction>,  "Test Search Tree Selection")
-        return node
+    /**
+     * Tests the [GenericSolver.select] method functionality without failure.
+     */
+    @Test fun coreLibraryTestSelectMethod(): ActionNode<TestStochasticState, TestStochasticAction>  {
+        return solver.select(testRoot)
     }
 
-    // Ensure expansion is working properly, and meta-info is transfered from state-to-state.
+    /**
+     * Tests the [GenericSolver.simulate] method functionality without failure.
+     */
+    @Test fun coreLibraryTestSimulation() {
+        solver.simulate(testRoot)
+    }
+
+    /**
+     * Tests the both the solver [GenericSolver.expand] and [GenericSolver.simulate]
+     * basic functionality working in unison.
+     */
+    @Test fun coreLibraryTestExpandAndSelect(): ActionNode<TestStochasticState, TestStochasticAction> {
+        return solver.select(coreLibraryTestExpand())
+    }
+
+    /**
+     * Tests to ensure the [GenericSolver.expand] method is working properly,
+     * and correct meta-info is transfered from state-to-state.
+     */
     @Test fun coreLibraryTestExpand(): ActionNode<TestStochasticState, TestStochasticAction> {
         var nextNode = solver.expand(testRoot)
         val iterC = (2..99).random()
@@ -54,51 +73,46 @@ class CoreLibraryTests {
         return nextNode
     }
 
-    // Test selection returns an action node.
-    @Test fun coreLibraryTestSelection2(): ActionNode<TestStochasticState, TestStochasticAction> {
-        val node = solver.select(coreLibraryTestExpand())
-        assertTrue(node is ActionNode<TestStochasticState, TestStochasticAction>,  "Test Search Tree Selection")
-        return node
-    }
-
-    // Test simulation returns a valid numeric reward
-    @Test fun coreLibraryTestSimulation() {
-        val rewardValue = solver.simulate(testRoot)
-        assertTrue(rewardValue is Double,  "Test Simulation")
-    }
-
-    // Test backpropagation function increment
+    /**
+     * Tests to ensure the backpropagation method backpropagates values properly. In this test the monotonic relation of
+     * n visits from child to parent is always maintained.
+     */
     @Test fun coreLibraryTestBackpropagation() {
         var node = testRoot
         val n1 = node.n
         solver.backpropagate(testRoot, 20.0)
         val n2 = node.n
-        println("n2: " + n2.toString())
-        println("n1: " + n1.toString())
-        assertTrue(n2 > n1,  "Test Increment of Node Visits")
+        // println("n2: " + n2.toString())
+        // println("n1: " + n1.toString())
+        assertTrue(n2 >= n1,  "Monotonic guarantee of n child <= n parent in single Backpropagate")
     }
 
-    // Tests for the base Solver
-
-    // Test all key mechanisms, run single iteration
+    // Base Solver Testing
+    /**
+     * Test all key mechanisms, run single iteration without failure.
+     */
     @Test fun coreLibraryTestSingleIteration() {
         solver.runTreeSearchIteration()
     }
 
-    // Test MC Tree Search, test if counted n times properly
+    /**
+     * Test to ensure the MCTS algorithm as a whole is running, and gurantees a monotonic relationship from child node
+     * to parent note, where n child >= n parent for any random traversal down the tree. This is a gurantee for any
+     * MCTS algorithm.
+     */
     @Test fun coreLibraryTestMCTS() {
         solver.runTreeSearch(99)
         val rootN = testRoot.n
         println("n root: "+ rootN.toString())
 
-        var nextNodes = testRoot.getChildren() ?: return
+        var nextNodes = testRoot.getChildren()
         var n1 = testRoot.n
         while(!nextNodes.isEmpty()){
             var nextNode = nextNodes.random()
             var n2 = nextNode.n
-            println("next node n: " + n2.toString())
+            // println("next node n: " + n2.toString())
             nextNodes = nextNode.getChildren()
-            assertTrue(n2 < n1,  "Test Increment of Node Visits")
+            assertTrue(n2 < n1,  "Monotonic guarantee of n child <= n parent in MCTS")
             n1 = n2
         }
     }
